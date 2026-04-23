7d482a1956df28e82bb0f8ad56bfff154effb4e9
Björn Ritzl <bjorn.ritzl@gmail.com>
2026-04-23 23:42:34 +0200

    Stop using deprecated collectionfactory.missing_resources() function (#125)
    * Use get_resources
    
    * Update monarch.lua

--- monarch/monarch.lua
+++ monarch/monarch.lua
@@ -1110,8 +1110,15 @@ function M.has_missing_resources(id)
 
 	local screen = screens[id]
 	if screen.proxy then
-		local missing_resources = collectionproxy.missing_resources(screen.proxy)
-		return #missing_resources > 0
+		if not collectionproxy.missing_resources then
+			-- from defold 1.13.0
+			local resources = collectionproxy.get_resources(screen.proxy)
+			return #resources == 0
+		else
+			-- for backwards compatibility
+			local missing = collectionproxy.missing_resources(screen.proxy)
+			return #missing > 0
+		end
 	end
 	return false
 end
